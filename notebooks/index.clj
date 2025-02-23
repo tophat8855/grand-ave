c140c3b8b3155536a016c278d226f4af37bfeb82
Heather Moore-Farley <hmoorefarley@protonmail.com>
2025-02-23 15:46:13 -0800

    some basic bar charts

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -1,11 +1,16 @@
 ^{:kindly/hide-code true
   :kindly/kind      :kind/hiccup}
 (ns index
-  (:require [camel-snake-kebab.core :as csk]
+  (:require [aerial.hanami.templates :as ht]
+            [camel-snake-kebab.core :as csk]
             [scicloj.kindly.v4.kind :as kind]
+            [scicloj.tableplot.v1.plotly :as plotly]
             [scicloj.tableplot.v1.hanami :as hanami]
             [tablecloth.api :as tc]
+            [tablecloth.column.api :as tcc]
             [tech.v3.dataset :as ds]))
+^{:kindly/hide-code true}
+(import java.time.LocalDateTime)
 
 ^{:kindly/hide-code true
   :kindly/kind      :kind/hiccup}
@@ -38,6 +43,7 @@
 (defn load-and-combine-csvs [file-paths]
   (let [datasets (map #(ds/->dataset % {:key-fn    csk/->kebab-case-keyword
                                         :parser-fn {:collision-id       :integer
+                                                    :crash-date-time    :local-date-time
                                                     :ncic-code          :integer
                                                     :is-highway-related :boolean
                                                     :is-tow-away        :boolean
@@ -46,37 +52,64 @@
                       file-paths)]
     (apply ds/concat datasets)))
 
-(-> (load-and-combine-csvs csv-files)
-    (ds/select-columns [:collision-id
-                        :ncic-code
-                        :crash-date-time
-                        :collision-type-description
-                        :day-of-week
-                        :is-highway-related
-                        :motor-vehicle-involved-with-desc
-                        :motor-vehicle-involved-with-other-desc
-                        :number-injured
-                        :number-killed
-                        :lighting-description
-                        :latitude
-                        :longitude
-                        :pedestrian-action-desc
-                        :primary-road
-                        :secondary-road])
-    (ds/filter #(clojure.string/includes? (or (:primary-road %)
-                                              (:secondary-road %)) "GRAND"))
-    (ds/filter (fn [row]
-                 (or (some #(clojure.string/includes? (:primary-road row) %)
-                           intersections-of-interest)
-                     (some #(clojure.string/includes? (:secondary-road row) %)
-                           intersections-of-interest))))
-    (tc/dataset))
+(def grand-ave-crashes
+  (-> (load-and-combine-csvs csv-files)
+      (ds/select-columns [:collision-id
+                          :ncic-code
+                          :crash-date-time
+                          :collision-type-description
+                          :day-of-week
+                          :is-highway-related
+                          :motor-vehicle-involved-with-desc
+                          :motor-vehicle-involved-with-other-desc
+                          :number-injured
+                          :number-killed
+                          :lighting-description
+                          :latitude
+                          :longitude
+                          :pedestrian-action-desc
+                          :primary-road
+                          :secondary-road])
+      (ds/filter #(clojure.string/includes? (or (:primary-road %)
+                                                (:secondary-road %)) "GRAND"))
+      (ds/filter (fn [row]
+                   (or (some #(clojure.string/includes? (:primary-road row) %)
+                             intersections-of-interest)
+                       (some #(clojure.string/includes? (:secondary-road row) %)
+                             intersections-of-interest))))))
 
-#_(-> grand-ave-crashes
-    (hanami/plot hanami/line-chart
-                 {:x :crash-date-time
-                  :y :number-injured
-                  :color :collision-type-description
-                  :title "Injuries by Crash Type"
-                  :width 800
-                  :height 400}))
+(-> grand-ave-crashes
+    (tc/dataset)
+    (plotly/layer-bar
+     {:=x :crash-date-time
+      :=y :number-injured}))
+
+(-> grand-ave-crashes
+    (ds/row-map (fn [row]
+                  (let [date-time (:crash-date-time row)]
+                    (assoc row
+                           :month-year (str (.getYear date-time) "-" (.getMonthValue date-time))))))
+    (tc/dataset)
+    (plotly/layer-bar
+     {:=x :month-year
+      :=y :number-injured}))
+
+(-> grand-ave-crashes
+    (ds/row-map (fn [row]
+                  (let [date-time (:crash-date-time row)]
+                    (assoc row
+                           :year (str (.getYear date-time))))))
+    (tc/dataset)
+    (plotly/layer-bar
+     {:=x :year
+      :=y :number-injured}))
+
+(-> grand-ave-crashes
+    (ds/row-map (fn [row]
+                  (let [date-time (:crash-date-time row)]
+                    (assoc row
+                           :year (str (.getYear date-time))))))
+    (tc/dataset)
+    (plotly/layer-bar
+     {:=x :year
+      :=y :number-killed}))
